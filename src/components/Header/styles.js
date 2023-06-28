da4dec1c9e7d7df30fa47a327e5815692a1c25f2
Felipe Luiz de Lima <130179132+felipiius13@users.noreply.github.com>
2023-06-28 12:30:12 -0300

    Add files via upload

---
+++ src/components/Header/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 150px;
+  text-align: center;
+  background: linear-gradient(180deg, #232323,#ff0000,#232323);
+`;
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+  padding-top: 20px;
+  color: #fff;
+`;
